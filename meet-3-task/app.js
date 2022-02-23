f8494f8df58f2b8fec3e2a5900955b0ba89f3057
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-02-23 21:13:23 +0700

    feat: mongoDB setup

--- meet-3-task/app.js
+++ meet-3-task/app.js
@@ -1,21 +1,28 @@
 import express from "express";
-
+//import mongoose from "mongoose";
+require("dotenv").config();
 import usersRoutes from './routes/users.js';
+//import connectDB from './db/connect';
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
-
+app.use(express.urlencoded({extended : false}));
 app.use('/users', usersRoutes);
 
 app.get('/', (req, res) => {
     res.send('Nothing here, go to /users');
 });
 
+const start = async (req, res) => {
+    try{
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, console.log(`Server running on port: http://localhost:${PORT}`));
+    }catch(err){
+        console.log(err);
+    }
+}
 
-
-app.listen(PORT, ()=>{
-    console.log(`Server running on port: http://localhost:${PORT}`);
-});
+start();
 
