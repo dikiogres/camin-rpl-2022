dbf65ddec91273a28cfc4fd0b3ad69e5813d0566
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-02-23 21:32:44 +0700

    add: user.js for validation

---
+++ meet-3-task/models/user.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('user', userSchema);
