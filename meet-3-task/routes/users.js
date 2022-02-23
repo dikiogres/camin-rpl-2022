0c6a9700c9aba95c22715ec8bffab1647f9db720
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-02-23 20:20:16 +0700

    add: meet-3-task initiation

---
+++ meet-3-task/routes/users.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+import { 
+    getAllUsers, 
+    createUser, 
+    getUser, 
+    deleteUser, 
+    updateUser 
+} from '../controllers/users.js'
+
+const router = express.Router();
+
+router.get('/', getAllUsers)
+
+router.post('/', createUser);
+
+router.get('/:id', getUser);
+
+router.delete('/:id', deleteUser);
+
+router.patch('/:id', updateUser);
+
+export default router;
